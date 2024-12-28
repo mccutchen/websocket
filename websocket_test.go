ec99f86fe077380a2274f516dbe13a0c272e2754
Will McCutchen <will@mccutch.org>
2024-12-28 12:27:14 -0500

    test: add helpers to simplify writing unit test for protocol basics (#11)

--- websocket_test.go
+++ websocket_test.go
@@ -10,7 +10,6 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"os"
-	"strings"
 	"sync"
 	"testing"
 	"time"
@@ -156,6 +155,8 @@ func TestAccept(t *testing.T) {
 		}
 
 		t.Run("http.Hijack not implemented", func(t *testing.T) {
+			t.Parallel()
+
 			// confirm that httptest.ResponseRecorder does not implmeent
 			// http.Hjijacker
 			var w http.ResponseWriter = httptest.NewRecorder()
@@ -173,6 +174,8 @@ func TestAccept(t *testing.T) {
 		})
 
 		t.Run("hijack failed", func(t *testing.T) {
+			t.Parallel()
+
 			defer func() {
 				r := recover()
 				if r == nil {
@@ -186,21 +189,15 @@ func TestAccept(t *testing.T) {
 }
 
 func TestConnectionLimits(t *testing.T) {
-	// TODO: need to rework these tests in terms of read/write deadlines rather
-	// than total request time.
 	t.Run("server enforces read deadline", func(t *testing.T) {
 		t.Parallel()
 
 		maxDuration := 250 * time.Millisecond
-
-		srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		_, conn := setupRawConn(t, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			ws, err := websocket.Accept(w, r, websocket.Options{
 				ReadTimeout:  maxDuration,
 				WriteTimeout: maxDuration,
-				// TODO: test these limits as well
-				MaxFragmentSize: 128,
-				MaxMessageSize:  256,
-				Hooks:           newTestHooks(t),
+				Hooks:        newTestHooks(t),
 			})
 			if err != nil {
 				http.Error(w, err.Error(), http.StatusBadRequest)
@@ -208,64 +205,20 @@ func TestConnectionLimits(t *testing.T) {
 			}
 			ws.Serve(r.Context(), websocket.EchoHandler)
 		}))
-		defer srv.Close()
 
-		conn, err := net.Dial("tcp", srv.Listener.Addr().String())
+		// client read should succeed, because we expect the server to send
+		// a close frame when its read deadline is reached
+		start := time.Now()
+		resp, err := io.ReadAll(conn)
+		elapsed := time.Since(start)
 		assert.NilError(t, err)
-		defer conn.Close()
+		assert.RoughlyEqual(t, elapsed, maxDuration, 25*time.Millisecond)
+		validateCloseFrame(t, bytes.NewBuffer(resp), websocket.StatusServerError, "timeout")
 
-		// first, we write the request line and headers, which should cause the
-		// server to respond with a 101 Switching Protocols response.
-		{
-			reqParts := []string{
-				"GET /websocket/echo HTTP/1.1",
-				"Host: test",
-				"Connection: upgrade",
-				"Upgrade: websocket",
-				"Sec-WebSocket-Key: dGhlIHNhbXBsZSBub25jZQ==",
-				"Sec-WebSocket-Version: 13",
-			}
-			reqBytes := []byte(strings.Join(reqParts, "\r\n") + "\r\n\r\n")
-			t.Logf("raw request:\n%q", reqBytes)
-
-			n, err := conn.Write(reqBytes)
-			assert.NilError(t, err)
-			assert.Equal(t, n, len(reqBytes), "incorrect number of bytes written")
-
-			resp, err := http.ReadResponse(bufio.NewReader(conn), nil)
-			assert.NilError(t, err)
-			assert.StatusCode(t, resp, http.StatusSwitchingProtocols)
-		}
-
-		// next, we try to read from the connection, expecting the connection
-		// to be closed after roughly maxDuration seconds
-		{
-			start := time.Now()
-			resp, err := io.ReadAll(conn)
-			elapsed := time.Since(start)
-
-			// client read should succeed, because we expect the server to send
-			// a close frame when its read deadline is reached
-			assert.NilError(t, err)
-			assert.RoughlyEqual(t, elapsed, maxDuration, 25*time.Millisecond)
-
-			frame, err := websocket.ReadFrame(bytes.NewBuffer(resp))
-			assert.NilError(t, err)
-			assert.Equal(t, frame.Opcode, websocket.OpcodeClose, "expected close frame")
-
-			// extract close status code and message from payload
-			assert.Equal(t, len(frame.Payload) >= 2, true, "expected close frame payload to be at least 2 bytes, got %v", frame.Payload)
-			statusCode := websocket.StatusCode(binary.BigEndian.Uint16(frame.Payload[:2]))
-			closeMsg := string(frame.Payload[2:])
-			t.Logf("got close frame: code=%v msg=%q", statusCode, closeMsg)
-			assert.Equal(t, statusCode, websocket.StatusServerError, "expected server error status code")
-			assert.Contains(t, closeMsg, "timeout", "expected timeout close message")
-
-			// connection should be closed, so we should get EOF when trying to
-			// read from it again
-			_, err = conn.Read(make([]byte, 1))
-			assert.Error(t, err, io.EOF)
-		}
+		// connection should be closed, so we should get EOF when trying to
+		// read from it again
+		_, err = conn.Read(make([]byte, 1))
+		assert.Error(t, err, io.EOF)
 	})
 
 	t.Run("client closing connection", func(t *testing.T) {
@@ -287,7 +240,7 @@ func TestConnectionLimits(t *testing.T) {
 		)
 
 		wg.Add(1)
-		srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		_, conn := setupRawConn(t, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			defer wg.Done()
 			start := time.Now()
 			ws, err := websocket.Accept(w, r, websocket.Options{
@@ -307,66 +260,92 @@ func TestConnectionLimits(t *testing.T) {
 			ws.Serve(r.Context(), websocket.EchoHandler)
 			elapsedServerTime = time.Since(start)
 		}))
-		defer srv.Close()
-
-		conn, err := net.Dial("tcp", srv.Listener.Addr().String())
-		assert.NilError(t, err)
-		defer conn.Close()
 
 		// should cause the client end of the connection to close well before
 		// the max request time configured above
 		assert.NilError(t, conn.SetDeadline(time.Now().Add(clientTimeout)))
 
-		// first, we write the request line and headers, which should cause the
-		// server to respond with a 101 Switching Protocols response.
-		{
-			reqParts := []string{
-				"GET /websocket/echo HTTP/1.1",
-				"Host: test",
-				"Connection: upgrade",
-				"Upgrade: websocket",
-				"Sec-WebSocket-Key: dGhlIHNhbXBsZSBub25jZQ==",
-				"Sec-WebSocket-Version: 13",
-			}
-			reqBytes := []byte(strings.Join(reqParts, "\r\n") + "\r\n\r\n")
-			t.Logf("raw request:\n%q", reqBytes)
-
-			n, err := conn.Write(reqBytes)
-			assert.NilError(t, err)
-			assert.Equal(t, n, len(reqBytes), "incorrect number of bytes written")
-
-			resp, err := http.ReadResponse(bufio.NewReader(conn), nil)
-			assert.NilError(t, err)
-			assert.StatusCode(t, resp, http.StatusSwitchingProtocols)
-			t.Logf("handshake completed")
-		}
-
-		// next, we try to read from the connection, expecting the connection
+		// try to read from the connection, expecting the connection
 		// to be closed after roughly clientTimeout seconds.
 		//
 		// the server should detect the closed connection and abort the
 		// handler, also after roughly clientTimeout seconds.
-		{
-			start := time.Now()
-			_, err := conn.Read(make([]byte, 1))
-			elapsedClientTime = time.Since(start)
+		start := time.Now()
+		_, err := conn.Read(make([]byte, 1))
+		elapsedClientTime = time.Since(start)
 
-			// close client connection, which should interrupt the server's
-			// blocking read call on the connection
-			conn.Close()
-			t.Logf("client connection closed")
+		// close client connection, which should interrupt the server's
+		// blocking read call on the connection
+		conn.Close()
+		t.Logf("client connection closed")
 
-			assert.Equal(t, os.IsTimeout(err), true, "expected timeout error")
-			assert.RoughlyEqual(t, elapsedClientTime, clientTimeout, 10*time.Millisecond)
+		assert.Equal(t, os.IsTimeout(err), true, "expected timeout error")
+		assert.RoughlyEqual(t, elapsedClientTime, clientTimeout, 10*time.Millisecond)
 
-			// wait for the server to finish handling the one request, then
-			// make sure it took the expected amount of time to get a read
-			// timeout and an appropriate error
-			wg.Wait()
-			assert.RoughlyEqual(t, elapsedServerTime, clientTimeout, 10*time.Millisecond)
-			assert.Error(t, gotServerReadError, io.EOF)
-		}
+		// wait for the server to finish handling the one request, then
+		// make sure it took the expected amount of time to get a read
+		// timeout and an appropriate error
+		wg.Wait()
+		assert.RoughlyEqual(t, elapsedServerTime, clientTimeout, 10*time.Millisecond)
+		assert.Error(t, gotServerReadError, io.EOF)
 	})
+}
+
+// setupRawConn is a test helpers that runs a test server and does the
+// initial websocket handshake. The returned connection is ready for use to
+// sent/receive websocket messages.
+func setupRawConn(t *testing.T, handler http.Handler) (*httptest.Server, net.Conn) {
+	t.Helper()
+
+	srv := httptest.NewServer(handler)
+	conn, err := net.Dial("tcp", srv.Listener.Addr().String())
+	assert.NilError(t, err)
+
+	t.Cleanup(func() {
+		conn.Close()
+		srv.Close()
+	})
+
+	handshakeReq := httptest.NewRequest(http.MethodGet, "/", nil)
+	for k, v := range map[string]string{
+		"Connection":            "upgrade",
+		"Upgrade":               "websocket",
+		"Sec-WebSocket-Key":     "dGhlIHNhbXBsZSBub25jZQ==",
+		"Sec-WebSocket-Version": "13",
+	} {
+		handshakeReq.Header.Set(k, v)
+	}
+	// write the request line and headers, which should cause the
+	// server to respond with a 101 Switching Protocols response.
+	assert.NilError(t, handshakeReq.Write(conn))
+	resp, err := http.ReadResponse(bufio.NewReader(conn), nil)
+	assert.NilError(t, err)
+	assert.StatusCode(t, resp, http.StatusSwitchingProtocols)
+
+	return srv, conn
+}
+
+// validateCloseFrame ensures that we can read a close frame from the given
+// reader and optionally ensures that the close frame includes a specific
+// status code and message.
+func validateCloseFrame(t *testing.T, r io.Reader, wantStatus websocket.StatusCode, wantMsg string) {
+	t.Helper()
+
+	frame, err := websocket.ReadFrame(r)
+	assert.NilError(t, err)
+	assert.Equal(t, frame.Opcode, websocket.OpcodeClose, "expected close frame")
+
+	if wantStatus == 0 {
+		// nothing else to validate
+		return
+	}
+
+	assert.Equal(t, len(frame.Payload) >= 2, true, "expected close frame payload to be at least 2 bytes, got %v", frame.Payload)
+	statusCode := websocket.StatusCode(binary.BigEndian.Uint16(frame.Payload[:2]))
+	closeMsg := string(frame.Payload[2:])
+	t.Logf("got close frame: code=%v msg=%q", statusCode, closeMsg)
+	assert.Equal(t, statusCode, websocket.StatusServerError, "got incorrect close status code")
+	assert.Contains(t, closeMsg, wantMsg, "got incorrect close message")
 }
 
 // brokenHijackResponseWriter implements just enough to satisfy the
